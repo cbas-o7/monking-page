59a18c55d586bf08877fcdc8f48f499ab44d56da
Sebastian <erick.cortillo@gmail.com>
2025-05-02 00:21:51 -0600

    Arreglo de errores para la build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Header from '@/components/header';
+import Header from '@/components/Header';
 import { getData } from "@/src/firebase"
 import { useState, useEffect, useCallback, useMemo, memo } from "react"
 import { Badge } from "@/components/ui/badge"
